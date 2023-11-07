ad0551cf249de6b925351c9e247cec97f2e5d1b3
ravi406 <ravikamble406@gmail.com>
2023-11-07 18:06:17 +0530

    Added search filter and router is implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,77 @@
 import RestaurentCard from "./RestaurentCard";
-import restaurantData from "../../utils/mockData";
 import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 const Body =() =>{
 
+  //local state variable -super powerfull variable
+  const [searchText,setSearchText]=useState(' ');
+  const [listOfRestaurant, setListOfRestaurant ] = useState([]);
+  const [filteredListOfRestaurant,setFilteredRestaurant] = useState('');
+
+  //whenever state variable changes react triggers a reconciliation cycle(re-render the component )
+  console.log('Body rendering..');
+  // console.log(listOfRestaurant);
+  
+  useEffect(() => {  
+    
+    fetchData();
+
+    },[]);       
+
+    const fetchData = async () =>{
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9686706&lng=77.5300547&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const json = await data.json();
+        setListOfRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+          .map((res)=>res.info));
+
+        setFilteredRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+          .map((res)=>res.info));
+        
+    }
+
    
 
-    const [ListOfRestaurant, setListOfRestaurant ] = useState(restaurantData);
+    
 
     function clickHandler(){
       // filter out the restaurants based on the rating
-      const filteredList = ListOfRestaurant.filter((rest) => rest.avgRating > 4.2 );
+      const filteredList = listOfRestaurant.filter((rest) => rest.avgRating > 4.2 );
       setListOfRestaurant(filteredList);
  
     }            
         
-    return(
+    return listOfRestaurant.length === 0 ? <Shimmer/> : (
         <div className='body'>
             <div className='filter'>
-              <button type='button' className="filter-btn" onClick={clickHandler}>Top-Rated-Restaurants</button>
+            <div className="search">
+              <input type="text" placeholder="Search Restaurant" 
+              value={searchText} 
+              onChange={(event)=>{
+                  setSearchText(event.target.value);
+              }}/>
+              <button onClick={()=>{
+                //filter the restaurant cards and update the UI
+                //searchText  
+
+                const filteredRestaurant = listOfRestaurant.filter((restaurant) =>{
+                     return restaurant.name.toLowerCase().includes(searchText.toLowerCase());
+      
+                })
+
+               setFilteredRestaurant(filteredRestaurant); 
+
+                
+              }}>Search</button>
             </div>
+              <button type='button' 
+                className="filter-btn" onClick={clickHandler}>Top-Rated-Restaurants</button>
+            </div>
+            
             <div className='resto-container'>
               {
-                ListOfRestaurant.map(restaurant => <RestaurentCard key={restaurant.id} resData ={restaurant}/>)
+                filteredListOfRestaurant.map(restaurant => <RestaurentCard key={restaurant.id} resData ={restaurant}/>)
               } 
                 
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,37 @@
+
 import { LOGO_URL } from "../../utils/constants";
+import { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Header =() =>{
+
+     const [btnName,setBtnName] = useState('Login');
+
+    useEffect(() => {console.log('useeffect called with every render')});
+    useEffect(() => {console.log('useeffect called with only one render')},[btnName]);
+
+   
+
     return(
         <div className='header'>
+            
             <div className='logo-container'>
                 <img className="logo" alt="Logo" src={LOGO_URL}/>                
             </div>
 
             <div className='nav-items'>
                 <ul>
-                    <li>Home</li>
-                    <li>Contact us</li>
-                    <li>About-us</li>
+                    
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/contact'>Contact-us</Link></li>
+                    <li><Link to='/about'>About-us</Link></li>
                     <li>Cart</li>
+                    <button onClick={()=>
+                    {
+                        btnName ==='Login' ? setBtnName('Logout') :setBtnName('Login');
+                    }}>{btnName}</button>
                 </ul>
+
             </div>
 
         </div>
